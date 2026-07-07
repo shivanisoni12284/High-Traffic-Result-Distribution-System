4b659e47f39a6d8b8fe3d81e7ce397b37ba824e1
Shivani Soni <shivanisoni12284@gmail.com>
2026-07-07 11:12:41 +0530

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/shivani/hightrafficresultdistributionsystem/HighTrafficResultDistributionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shivani.hightrafficresultdistributionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighTrafficResultDistributionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighTrafficResultDistributionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivani/hightrafficresultdistributionsystem/HighTrafficResultDistributionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivani.hightrafficresultdistributionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighTrafficResultDistributionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
